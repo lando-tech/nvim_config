f6381f63c31eb5493d003b7cd4111e08409e5fee
Aaron <157839802+lando-tech@users.noreply.github.com>
2024-09-05 16:37:51 +0900

    Initial commit

---
+++ init.lua
@@ -0,0 +1,43 @@
+-- Packer configuration to manage plugins
+-- Ensure packer is installed
+vim.cmd [[packadd packer.nvim]]
+
+require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- File tree plugin
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end
+  }
+end)
+
+-- Enable line numbers
+vim.wo.number = true
+
+-- Map jj to exit Insert mode and go to Normal mode
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true })
+
+-- Set up a keybind to toggle the file tree (Ctrl + n)
+vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
+-- Optional: Set relative line numbers (if you prefer)
+-- vim.wo.relativenumber = true
+
+-- Switch to the next buffer
+vim.api.nvim_set_keymap('n', '<leader>n', ':bnext<CR>', { noremap = true, silent = true })
+
+-- Switch to the previous buffer
+vim.api.nvim_set_keymap('n', '<leader>p', ':bprevious<CR>', { noremap = true, silent = true })
+
+vim.cmd('NvimTreeOpen /home/landotech/Documents/Github/')
+
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+
+
