7b9be342889cf3c433ffb1c8d78f4643710cc427
lando-tech <landotek@proton.me>
2024-09-23 00:27:37 +0900

    updated theme

--- lua/themes/rocketman_theme.lua
+++ lua/themes/rocketman_theme.lua
@@ -1,88 +1,146 @@
--- Set theme options
+-- ColorScheme
+local black = "#131319"
+local white = "#eceff1"
+local light_grey = "#e3e8eb"
+local grey = "#90a4ae"
+local blue = "#7fb3d5"
+local green = "#7dcea0"
+local purple = "#7e57c2"
+local light_purple = "#ce93d8"
+local pink = "#ce93d8"
+local bg_grey = "#263238"
+local bg_2 = "#313136"
+local dark_red = "#cc3359"
+local lime = "#dce775"
+local dark_orange = "#ff6e40" 
+local violet = "#7986cb"
+local white_grey = "#e2e2e2"
+local olive = "#afb42b"
+local cyan_1 = "#00838f"
 
--- Editor colors
--- vim.cmd("highlight Normal guibg=1e292f guifg=#d3dcd0")  -- Background and foreground
--- vim.cmd("highlight Visual guibg=#3C3C58")  -- Selection background
--- vim.cmd("highlight Cursor guifg=#1B1B23 guibg=#FFA07A")  -- Caret color (cursor)
--- vim.cmd("highlight CursorLine guibg=#2A2A3B")  -- Line highlight
---
--- -- Syntax colors
--- vim.cmd("highlight Comment guifg=#666699")  -- Comments
--- vim.cmd("highlight Keyword guifg=#569fd7")
--- vim.cmd("highlight String guifg=#75b98a")
--- vim.cmd("highlight Identifier guifg=#d78e56")
--- vim.cmd("highlight Type guifg=#FFC300 ")
--- vim.cmd("highlight Number guifg=#cf56d7")
--- vim.cmd("highlight Constant guifg=#cf56d7")
--- vim.cmd("highlight Conditional guifg=#f5ece6")
--- vim.cmd("highlight Repeat guifg=#e6eff5")
--- vim.cmd("highlight Statement guifg=#569fd7")
--- vim.cmd("highlight Punctuation guifg=#569fd7")
--- vim.cmd("highlight Delimiter guifg=#569fd7")
---
--- -- Popup menu background and foreground
--- vim.cmd("highlight Pmenu guibg=#2E3440 guifg=#D8DEE9")
---
--- -- Popup menu selected item
--- vim.cmd("highlight PmenuSel guibg=#88C0D0 guifg=#2E3440")
---
--- -- Popup menu scrollbar
--- vim.cmd("highlight PmenuSbar guibg=#3B4252")
--- vim.cmd("highlight PmenuThumb guibg=#81A1C1")
---
--- -- Gutter (line numbers)
--- vim.cmd("highlight LineNr guibg=#1F1F28 guifg=#d0d9dc ")
---
--- -- Scrollbar colors
--- vim.cmd("highlight ScrollbarThumb guibg=#4E4E6A")
--- vim.cmd("highlight Scrollbar guibg=#2C2C3C")
---
--- -- StatusBar (you can use lualine or another statusline plugin)
--- vim.cmd("highlight StatusLine guibg=#2A2A3A guifg=#B0B0B8")
--- vim.cmd("highlight NvimTreeRootFolder guifg=#FF5733")
--- vim.cmd("highlight NvimTreeFolderName guifg=#B19CD9")
--- vim.cmd("highlight EndOfBuffer guifg=#900C3F")
-
--- ~/.config/nvim/lua/themes/rocketman_theme.lua
 local lush = require('lush')
 
 return lush(function()
   return {
-    Normal { fg = "#EAEAEA", bg = "#19191A" },  -- Editor background and foreground
-    Visual { bg = "#2D2D48" },  -- Selection background
-    Cursor { fg = "#0A0A0F", bg = "#FFB74D" },  -- Caret color
-    CursorLine { bg = "#161621" },  -- Line highlight
+    Normal { fg = white, bg = bg_2 },  -- Editor background and foreground
+    Visual { fg = black, bg = grey },  -- Selection background
+    Cursor { fg = white, bg = black },  -- Caret color
+    CursorLine { fg = black, bg = grey },  -- Line highlight
+    CursorLineNr { fg = black, bg = grey },
+    CursorColumn { fg = black, bg = grey },
+    ColorColumn { fg = white, bg = dark_red },
 
-    -- Syntax
-    Comment { fg = "#6D6D79" },
-    Keyword { fg = "#FF9900" },
-    String { fg = "#00ff55" },
-    Function { fg = "#9A86FD" },
-    Identifier { fg = "#E1E1FF" },
-    Number { fg = "#c10bb5" },
-    Type { fg = "#66b3ff" },
-    Title { fg = "#c94212"},
-    Constant { fg = "#F7D779" },
-    Statement { fg = "#cc3359"},
-    Special { fg = "#d26f2d"},
+    -- Comments 
+    Comment { fg = grey },
+    Keyword { fg = cyan_1 },
+    MatchParen { fg = black, bg = grey },
+
+    -- Buffer Lines
+    EndOfBuffer { fg = grey },
+
+    -- Strings
+    String { fg = grey },
+
+    -- Fold and Sign column
+    FoldColumn { fg = black, bg = grey },
+    Folded { fg = black, bg = grey },
+    SignColumn { fg = black, bg = grey },
+    WildMenu { fg = lime, bg = black },
+    Conceal { fg = white, bg = grey },
+
+    -- Functions
+    Function { fg = olive },
+    Identifier { fg = light_grey },
+    Type { fg = light_grey },
+    Title { fg = blue },
+
+    -- Constants
+    Constant { fg = blue },
+    Number { fg = blue },
+    Character { fg = grey },
+    Boolean { fg = olive },
+    Float { fg = blue },
+    
+    -- Statement
+    Statement { fg = cyan_1 },
+    Repeat { fg = cyan_1 },
+    Label { fg = light_grey },
+    Exception { fg = olive },
+
+    -- Special 
+    Special { fg = olive },
+    SpecialChar { fg = ""},
+    Delimiter { fg = ""},
+    SpecialComment { fg = ""},
+    Debug { fg = ""},
+    SpecialKey { fg = light_purple },
+
+    NonText { fg = grey },
+    Search { fg = lime, bg = black },
+    Tag { fg = light_grey },
+    Structure { fg = light_grey },
+    Operator { fg = "" },
+
+    -- File System
+    Directory { fg = grey, bg = black },
 
     -- Gutter
-    LineNr { fg = "#5F5F7B", bg = "#131319" },
+    LineNr { fg = grey, bg = black },
 
     -- Scrollbar
-    ScrollbarThumb { bg = "#3E3E5E" },
-    Scrollbar { bg = "#1C1C28" },
+    ScrollbarThumb { bg = grey },
+    Scrollbar { bg = black },
 
     -- Status line
-    StatusLine { fg = "#B5B5B5", bg = "#0F0F14" },
+    StatusLine { fg = grey , bg = black },
+    StatusLineNC { fg = grey , bg = black },
 
-    PreProc { fg = "#cc3359"},
+    -- Proc
+    PreProc { fg = violet },
+    Define { fg = white },
+    Macro { fg = dark_red },
+    PreCondit { fg = light_grey },
+    Include { fg = olive},
 
-    NvimTreeRootFolder { fg = "#99e6ff"},
+    -- Nvim Tree
+    NvimTreeRootFolder { fg = grey, bg = black },
+    NvimTreeFolderName { fg = grey, bg = black },
 
-    Pmenu { bg = "#141f1f"}
+    -- Nvim Internal
+    NvimInternalError { fg = white, bg = olive },
+    
+    -- Pmenu items
+    Pmenu { fg = grey, bg = black },
+    PmenuSel { fg = black, bg = grey },
+    PmenuThumb { fg = grey, bg = bg_2 },
+    PmenuSbar { fg = grey, bg = bg_2 },
 
-  }
+    -- Warnings, Errors, Messages
+    Error { fg = white, bg = dark_orange },
+    ErrorMsg { fg = white, bg = dark_orange },
+    WarningMsg { fg = dark_orange },
+    RedrawDebugClear { fg = lime, bg = black },
+    DiagnosticError { fg = dark_orange },
+    DiagnosticUnderlineError { cterm = false },
+    DiagnosticUnderlineWarn { cterm = false },
+    DiagnosticUnderlineInfo { cterm = false },
+    DiagnosticUnderlineHint { cterm = false },
+    Question { fg = white },
+    MoreMsg { fg = white },
+    RedrawBugRecompose { fg = white, bg = dark_red },
+    RedrawBugComposed { fg = white, bg = green },
+
+
+    -- Diff
+    DiffAdd { fg = white, bg = blue },
+    DiffChange { fg = white, bg = blue },
+    DiffDelete { fg = white, bg = dark_orange },
+    DiffText { fg = white, bg = dark_orange },
+
+    -- Tabline
+    Tabline { fg = white, bg = grey },
+    TablineFill { fg = white, bg = grey },
+
+}
 end)
-
 
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -159,14 +159,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: nvim-tree.lua
-time([[Config for nvim-tree.lua]], true)
-try_loadstring("\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14nvim-tree\frequire\0", "config", "nvim-tree.lua")
-time([[Config for nvim-tree.lua]], false)
 -- Config for: lush.nvim
 time([[Config for lush.nvim]], true)
 try_loadstring("\27LJ\2\nK\0\0\5\0\3\0\b6\0\0\0'\2\1\0B\0\2\0026\2\0\0'\4\2\0B\2\2\0A\0\0\1K\0\1\0\27themes.rocketman_theme\tlush\frequire-\1\0\4\0\3\0\0066\0\0\0009\0\1\0003\2\2\0)\3\0\0B\0\3\1K\0\1\0\0\rdefer_fn\bvim\0", "config", "lush.nvim")
 time([[Config for lush.nvim]], false)
+-- Config for: nvim-tree.lua
+time([[Config for nvim-tree.lua]], true)
+try_loadstring("\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14nvim-tree\frequire\0", "config", "nvim-tree.lua")
+time([[Config for nvim-tree.lua]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
