9371bde9bfb4a127b55d2ee873ad73f1386a30d2
lando-tech <landotek@proton.me>
2024-10-06 23:02:22 +0900

    removed archive

--- archive_lua/init.lua
+++
@@ -1,6 +0,0 @@
--- Load custom settings
-require('options')    -- General settings
-require('keymaps')    -- Keybindings
-require('plugins')    -- Plugins management
-require('lsp')        -- LSP settings 
-vim.cmd('colorscheme rocketman-theme')
