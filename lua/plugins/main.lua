caf6c038477cb25d3ce9787a0e9c5663dff0e9db
aaron newman <landotek@proton.me>
2025-04-15 12:24:33 +0900

    updates

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -1,21 +1,21 @@
-local greeting = {}
-greeting.loaded = false
-greeting.buf, greeting.win = nil, nil
+local notepad = {}
+notepad.loaded = false
+notepad.buf, notepad.win = nil, nil
 
-function greeting.launch_notepad()
-    if not greeting.loaded or not vim.api.nvim_win_is_valid(greeting.win) then
-        if not greeting.buf or not vim.api.nvim_buf_is_valid(greeting.buf) then
-        greeting.buf = vim.api.nvim_create_buf(false, true)
-        vim.api.nvim_set_option_value("bufhidden", "hide", { buf = greeting.buf})
-        vim.api.nvim_set_option_value("filetype", "markdown", { buf = greeting.buf})
-        vim.api.nvim_buf_set_lines(greeting.buf, 0, 1, false, {
+function notepad.launch_notepad()
+    if not notepad.loaded or not vim.api.nvim_win_is_valid(notepad.win) then
+        if not notepad.buf or not vim.api.nvim_buf_is_valid(notepad.buf) then
+        notepad.buf = vim.api.nvim_create_buf(false, true)
+        vim.api.nvim_set_option_value("bufhidden", "hide", { buf = notepad.buf})
+        vim.api.nvim_set_option_value("filetype", "markdown", { buf = notepad.buf})
+        vim.api.nvim_buf_set_lines(notepad.buf, 0, 1, false, {
             "#Notepad",
             "",
             "> Notepad clears when the current Neovim session closes"
         })
         end
 
-        greeting.win = vim.api.nvim_open_win(greeting.buf, true, {
+        notepad.win = vim.api.nvim_open_win(notepad.buf, true, {
             border = "rounded",
             relative = "editor",
             style = "minimal",
@@ -24,35 +24,35 @@ function greeting.launch_notepad()
             row = 1,
             col = math.ceil(vim.o.columns * 0.5),
         })
-        vim.api.nvim_set_option_value("winblend", 30, { win = greeting.win})
+        vim.api.nvim_set_option_value("winblend", 30, { win = notepad.win})
 
-        local keymaps_opts = { silent = true, buffer = greeting.buf }
+        local keymaps_opts = { silent = true, buffer = notepad.buf }
         vim.keymap.set("n", "<ESC>", function()
-            greeting.launch_notepad()
+            notepad.launch_notepad()
         end, keymaps_opts)
         vim.keymap.set("n", "q", function()
-        greeting.launch_notepad()
+        notepad.launch_notepad()
         end, keymaps_opts)
     else
-        vim.api.nvim_win_hide(greeting.win)
+        vim.api.nvim_win_hide(notepad.win)
     end
-    greeting.loaded = not greeting.loaded
-    return greeting
+    notepad.loaded = not notepad.loaded
+    return notepad
 end
 
-vim.keymap.set("n", "<leader>ll", greeting.launch_notepad, { noremap = true, desc = "Toggle Notepad"})
+vim.keymap.set("n", "<leader>ll", notepad.launch_notepad, { noremap = true, desc = "Toggle Notepad"})
 
-function greeting.run_build_panel()
+function notepad.run_build_panel()
     -- Setup or reuse buffer
-    if not greeting.buf or not vim.api.nvim_buf_is_valid(greeting.buf) then
-        greeting.buf = vim.api.nvim_create_buf(false, true)
-        vim.api.nvim_set_option_value("bufhidden", "hide", { buf = greeting.buf })
-        vim.api.nvim_set_option_value("filetype", "log", { buf = greeting.buf })
+    if not notepad.buf or not vim.api.nvim_buf_is_valid(notepad.buf) then
+        notepad.buf = vim.api.nvim_create_buf(false, true)
+        vim.api.nvim_set_option_value("bufhidden", "hide", { buf = notepad.buf })
+        vim.api.nvim_set_option_value("filetype", "log", { buf = notepad.buf })
     end
 
     -- (Re)open the floating window
-    if not greeting.loaded or not vim.api.nvim_win_is_valid(greeting.win) then
-        greeting.win = vim.api.nvim_open_win(greeting.buf, true, {
+    if not notepad.loaded or not vim.api.nvim_win_is_valid(notepad.win) then
+        notepad.win = vim.api.nvim_open_win(notepad.buf, true, {
             border = "rounded",
             relative = "editor",
             style = "minimal",
@@ -61,17 +61,17 @@ function greeting.run_build_panel()
             row = 1,
             col = math.ceil(vim.o.columns * 0.5),
         })
-        vim.api.nvim_set_option_value("winblend", 30, { win = greeting.win })
+        vim.api.nvim_set_option_value("winblend", 30, { win = notepad.win })
     else
-        vim.api.nvim_win_hide(greeting.win)
-        greeting.loaded = false
+        vim.api.nvim_win_hide(notepad.win)
+        notepad.loaded = false
         return
     end
 
-    greeting.loaded = true
+    notepad.loaded = true
 
     -- Clear buffer content
-    vim.api.nvim_buf_set_lines(greeting.buf, 0, -1, false, { "[Running build...]" })
+    vim.api.nvim_buf_set_lines(notepad.buf, 0, -1, false, { "[Running build...]" })
 
     -- Run the shell command asynchronously and write output into the buffer
     vim.fn.jobstart({ "sh", "-c", "make && ./bin/main" }, {
@@ -80,24 +80,24 @@ function greeting.run_build_panel()
         on_stdout = function(_, data)
             if data then
                 vim.schedule(function()
-                    vim.api.nvim_buf_set_lines(greeting.buf, -1, -1, false, data)
+                    vim.api.nvim_buf_set_lines(notepad.buf, -1, -1, false, data)
                 end)
             end
         end,
         on_stderr = function(_, data)
             if data then
                 vim.schedule(function()
-                    vim.api.nvim_buf_set_lines(greeting.buf, -1, -1, false, data)
+                    vim.api.nvim_buf_set_lines(notepad.buf, -1, -1, false, data)
                 end)
             end
         end,
         on_exit = function()
             vim.schedule(function()
-                vim.api.nvim_buf_set_lines(greeting.buf, -1, -1, false, { "", "[Build finished]" })
+                vim.api.nvim_buf_set_lines(notepad.buf, -1, -1, false, { "", "[Build finished]" })
             end)
         end,
     })
 end
 
-vim.keymap.set("n", "<F5>", greeting.run_build_panel, { desc = "Build & Run in IDE panel" })
+vim.keymap.set("n", "<F5>", notepad.run_build_panel, { desc = "Build & Run in IDE panel" })
 
